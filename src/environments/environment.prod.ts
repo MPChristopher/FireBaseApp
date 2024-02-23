4eafe0ea7f0cb182a91b8c38dd4ee88a483a3676
GOJuanIgnacio <nachobz200607@gmail.com>
2024-02-23 09:56:24 -0600

    Correcion firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBCZaV7ElIJ82W_XXpw8HABJwE_yp-B1Ok",
+    authDomain: "ngbytes-fireauth-9cc6c.firebaseapp.com",
+    projectId: "ngbytes-fireauth-9cc6c",
+    storageBucket: "ngbytes-fireauth-9cc6c.appspot.com",
+    messagingSenderId: "527061077133",
+    appId: "1:527061077133:web:b97fc589a9612d5187e6c4"
+  
+  }
 };
